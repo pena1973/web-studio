f22836ce4045c67bc54ee31eaa0b9eb784a422e8
pena1973 <penapisemnet@gmail.com>
2023-08-27 00:01:12 +0300

    ready

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -1,11 +1,11 @@
 import Layout from "@/components/layout/layout";
-
+import Link from 'next/link';
 export default function About() {
     return (
       <Layout>
         Контакты
-        <br />
-        <a href="/about"> Контакты отсутствуют перейдите на страницу компании </a>
+        <br />        
+        <Link href="/about"> Контакты отсутствуют перейдите на страницу компании </Link>
       </Layout>
     );
 }
